7630abae6982fa4f4430adef1ee51a896a284678
cuipengcheng <cuipengcheng@vjong.com>
2021-05-26 11:19:04 +0800

    Initial commit

---
+++ src/main/java/com/honghuotai/wisdomhrinterface/WisdomHrInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.honghuotai.wisdomhrinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomHrInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisdomHrInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/honghuotai/wisdomhrinterface/WisdomHrInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honghuotai.wisdomhrinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomHrInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
